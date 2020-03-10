d9241f78c955d85c8d1705e8b5114620d4b195b5
adm3wenyang <adm3wenyang@cascadeloans.local>
2020-03-10 11:38:36 -0700

    added fincal

---
+++ src/FinCal/BalanceCal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinCal
+{
+    public class BalanceCal
+    {
+        public long GetBalance(string accountId)
+        {
+            return 99999999999;
+        }
+    }
+}
